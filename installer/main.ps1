ec99ae67f12327cdde00919f41f8ea3976cd22cb
RollixCC <unionn.business@gmail.com>
2025-09-13 17:58:21 +0200

    Update main.ps1

--- installer/main.ps1
+++ installer/main.ps1
@@ -1,29 +1,41 @@
 Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "YCC Bootstrapper"
-$form.Width = 1000
-$form.Height = 600
+$form.Size = New-Object System.Drawing.Size(400, 150)
+$form.StartPosition = "CenterScreen"
+
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Loading..."
+$label.AutoSize = $true
+$label.Location = New-Object System.Drawing.Point(160, 20)
+$form.Controls.Add($label)
+
 $progress = New-Object System.Windows.Forms.ProgressBar
-$progress.Width = 800
-$progress.Height = 50
+$progress.Minimum = 0
+$progress.Maximum = 100
+$progress.Value = 0
 $progress.Style = 'Continuous'
-$progress.Value = 5
-$progress.Location = New-Object System.Drawing.Point(100,275)
+$progress.Size = New-Object System.Drawing.Size(350, 30)
+$progress.Location = New-Object System.Drawing.Point(20, 60)
 $form.Controls.Add($progress)
 
-$cancel = New-Object System.Windows.Forms.Button
-$cancel.Text = "Cancel"
-$cancel.Width = 100
-$cancel.Height = 30
-$cancel.Location = New-Object System.Drawing.Point(450, 350)
-$cancel.Add_Click({$form.Close()})
-$form.Controls.Add($cancel)
+$button = New-Object System.Windows.Forms.Button
+$button.Text = "Cancel"
+$button.Size = New-Object System.Drawing.Size(80, 30)
+$button.Location = New-Object System.Drawing.Point(160, 100)
+$button.Add_Click({ $form.Close() })
+$form.Controls.Add($button)
 
 $form.Show()
 
-for ($i=5; $i -le 100; $i++) {
+for ($i=0; $i -le 100; $i+=2) {
     Start-Sleep -Milliseconds 50
     $progress.Value = $i
+    [System.Windows.Forms.Application]::DoEvents()
 }
 
-[void]$form.ShowDialog()
+$label.Text = "Done!"
+Start-Sleep -Seconds 1
+$form.Close()
