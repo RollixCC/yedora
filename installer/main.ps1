37c3bf5ce5f37b2fc47461502563e2f25062158f
RollixCC <unionn.business@gmail.com>
2025-09-13 17:57:29 +0200

    Update main.ps1

--- installer/main.ps1
+++ installer/main.ps1
@@ -1,15 +1,24 @@
 Add-Type -AssemblyName System.Windows.Forms
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Loading Bar Example"
+$form.Text = "YCC Bootstrapper"
 $form.Width = 1000
 $form.Height = 600
 $progress = New-Object System.Windows.Forms.ProgressBar
 $progress.Width = 800
 $progress.Height = 50
-$progress.Value = 5
 $progress.Style = 'Continuous'
+$progress.Value = 5
 $progress.Location = New-Object System.Drawing.Point(100,275)
 $form.Controls.Add($progress)
+
+$cancel = New-Object System.Windows.Forms.Button
+$cancel.Text = "Cancel"
+$cancel.Width = 100
+$cancel.Height = 30
+$cancel.Location = New-Object System.Drawing.Point(450, 350)
+$cancel.Add_Click({$form.Close()})
+$form.Controls.Add($cancel)
+
 $form.Show()
 
 for ($i=5; $i -le 100; $i++) {
